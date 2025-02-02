0309936e0e3f2269413f5e11d975d87c7fb1f5be
GregCTWang <chiatien@uni.minerva.edu>
2025-02-01 18:13:28 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-gray-900 mb-8">
+          React + TypeScript + Vite
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App 
